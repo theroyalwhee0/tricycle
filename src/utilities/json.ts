8af3a6f8e8b2410d0beeb98fc420c5f9d8a18c9a
Adam Mill <hismajesty@theroyalwhee.com>
2022-07-23 15:10:00 -0400

    Change JsonObject to type (instead of interface). RestrictedContext pulls extend type from Context instead of assuming that it extends base type.

--- src/utilities/json.ts
+++ src/utilities/json.ts
@@ -1,6 +1,6 @@
 export type JsonPrimative = null | string | boolean | number
 export type JsonArray = JsonValue[]
 export type JsonValue = JsonPrimative | JsonObject | JsonArray
-export interface JsonObject {
+export type JsonObject = {
     [key: string]: JsonValue
 }
